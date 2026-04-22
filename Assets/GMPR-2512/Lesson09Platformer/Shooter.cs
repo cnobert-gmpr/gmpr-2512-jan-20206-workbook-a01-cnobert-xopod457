b298749038a78b483593320abfbf2fd9337ff3ac
xopod457 <xopod457@gmail.com>
2026-04-22 11:02:46 -0600

    final

--- Assets/GMPR-2512/Lesson09Platformer/Shooter.cs
+++ Assets/GMPR-2512/Lesson09Platformer/Shooter.cs
@@ -7,6 +7,7 @@ namespace GMPR2512.Lesson09Platformer
     {
         private Transform _lastObjectHit;
         [SerializeField] float _laserLenght = 8;
+        [SerializeField] float _rotaionSpeed = 50;
         private LineRenderer _laserLine;
         void Awake()
         {
@@ -25,19 +26,19 @@ namespace GMPR2512.Lesson09Platformer
             #region Rotaion
                 
 
-            float rotaionInput = 0;
+            //float rotaionInput = 0;
 
-            if (Input.GetKey(KeyCode.Comma))
-            {
-                rotaionInput = 200;
-            }
-            else if (Input.GetKey(KeyCode.Period))
-            {
-                rotaionInput = -200;
-            }
-            transform.parent.Rotate(new Vector3(0,0,rotaionInput*Time.deltaTime));
+            // if (Input.GetKey(KeyCode.Comma))
+            // {
+            //     rotaionInput = 200;
+            // }
+            // else if (Input.GetKey(KeyCode.Period))
+            // {
+            //     rotaionInput = -200;
+            // }
+            transform.parent.Rotate(new Vector3(0,0,_rotaionSpeed * Time.deltaTime));
             #endregion
-            int layerMask = LayerMask.GetMask("Ground","Enemy");
+            int layerMask = LayerMask.GetMask("Player");
             RaycastHit2D rh2d = Physics2D.Raycast(transform.position,-transform.right,_laserLenght,layerMask);
             Vector3 endPoint = transform.position + -transform.right * _laserLenght;
             if(rh2d.collider != null)
@@ -45,16 +46,17 @@ namespace GMPR2512.Lesson09Platformer
             _laserLine?.SetPosition(0,transform.position);
             _laserLine?.SetPosition(1,endPoint);
 
-
             if(rh2d.transform != null)
-            {
-                rh2d.transform.gameObject.GetComponent<Renderer>().material.color = Color.red;
-                if(_lastObjectHit != null && rh2d.transform != _lastObjectHit)
-                    _lastObjectHit.gameObject.GetComponent<Renderer>().material.color = Color.white;
-                _lastObjectHit = rh2d.transform;
-            }
-            else if(_lastObjectHit != null)
-                    _lastObjectHit.gameObject.GetComponent<Renderer>().material.color = Color.white;
+                Destroy(rh2d.transform.gameObject);
+            // if(rh2d.transform != null)
+            // {
+            //     rh2d.transform.gameObject.GetComponent<Renderer>().material.color = Color.red;
+            //     if(_lastObjectHit != null && rh2d.transform != _lastObjectHit)
+            //         _lastObjectHit.gameObject.GetComponent<Renderer>().material.color = Color.white;
+            //     _lastObjectHit = rh2d.transform;
+            // }
+            // else if(_lastObjectHit != null)
+            //         _lastObjectHit.gameObject.GetComponent<Renderer>().material.color = Color.white;
 
     }
 //        void OnrawGizmos()
